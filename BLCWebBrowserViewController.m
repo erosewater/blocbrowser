598156e1f6d6f9ec2dc1a0a76b7541a266b42e64
David Krasnove <krasnove@mac.com>
2014-12-09 11:50:56 -0800

    Converted touch handlers to gesture recognizers 2

---
+++ BLCWebBrowserViewController.m
@@ -0,0 +1,322 @@
+//
+//  BLCWebBrowserViewController.m
+//  BlocBrowser
+//
+//  Created by dbk-dev on 12/1/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import "BLCWebBrowserViewController.h"
+#import "BLCAwesomeFloatingToolbar.h"
+
+#define kBLCWebBrowserBackString NSLocalizedString(@"Back", @"Back command")
+#define kBLCWebBrowserForwardString NSLocalizedString(@"Forward", @"Forward command")
+#define kBLCWebBrowserStopString NSLocalizedString(@"Stop", @"Stop command")
+#define kBLCWebBrowserRefreshString NSLocalizedString(@"Refresh", @"Reload command")
+
+
+
+//#import "Reachability.h"
+//#import <SystemConfiguration/SystemConfiguration.h>
+
+@interface BLCWebBrowserViewController () <UIWebViewDelegate, UITextFieldDelegate, BLCAwesomeFloatingToolbarDelegate>
+
+
+
+@property (nonatomic, strong) UIWebView *webview;
+@property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) BLCAwesomeFloatingToolbar *awesomeToolbar;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
+
+@property (nonatomic, assign) NSUInteger frameCount;
+@property (nonatomic, assign) CGFloat zoomScale;
+@end
+
+@implementation BLCWebBrowserViewController
+
+#pragma mark - UIViewController
+
+
+
+- (void)loadView {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Welcome!", @"Welcome title")
+                                                    message:NSLocalizedString(@"Get excited to use the best web browser ever!", @"Welcome comment")
+                                                   delegate:nil
+                                          cancelButtonTitle:NSLocalizedString(@"OK, I'm excited!", @"Welcome button title") otherButtonTitles:nil];
+    [alert show];
+    
+    
+    UIView *mainView = [UIView new];
+    self.webview = [[UIWebView alloc] init];
+    self.webview.delegate = self;
+
+    
+
+    
+    self.textField = [[UITextField alloc] init];
+    self.textField.keyboardType = UIKeyboardTypeURL;
+    self.textField.returnKeyType = UIReturnKeyDone;
+    self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.textField.placeholder = NSLocalizedString(@"WebSite URL", @"Placeholder text for web browser URL field");
+    self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
+    self.textField.delegate = self;
+  
+    self.awesomeToolbar = [[BLCAwesomeFloatingToolbar alloc] initWithFourTitles:@[kBLCWebBrowserBackString, kBLCWebBrowserForwardString, kBLCWebBrowserStopString, kBLCWebBrowserRefreshString]];
+    self.awesomeToolbar.delegate = self;
+   
+
+
+    
+    for (UIView *viewToAdd in @[self.webview, self.textField, self.awesomeToolbar]) {
+        [mainView addSubview:viewToAdd];
+    }
+    
+    self.view = mainView;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+    self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.activityIndicator];
+    
+    
+    
+   
+    
+    // Do any additional setup after loading the view.
+    
+}
+
+
+
+
+
+- (void) viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    static const CGFloat itemHeight = 50;
+    CGFloat width = CGRectGetWidth(self.view.bounds);
+    CGFloat browserHeight = CGRectGetHeight(self.view.bounds) - itemHeight;
+    //CGFloat buttonWidth = CGRectGetWidth(self.view.bounds) / 4;
+    
+    // Now, assign the frames
+    self.textField.frame = CGRectMake(0, 0, width, itemHeight);
+    self.webview.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
+    
+    CGFloat toolBarPlace = browserHeight - 65;
+    
+  
+        
+    
+        
+    
+        
+    
+    
+    if (!_zoomScale) {
+        
+    
+    self.awesomeToolbar.frame = CGRectMake(20, toolBarPlace, 280, 60);
+    
+    } else {
+        self.awesomeToolbar.frame = CGRectMake(20, toolBarPlace, 280*_zoomScale, 60*_zoomScale);
+    }
+    
+}
+
+#pragma mark - BLCAwesomeFloatingToolbarDelegate
+
+- (void) floatingToolbar:(BLCAwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title {
+    //if ([title isEqual:NSLocalizedString(@"Back", @"Back command")]) {
+    if ([title isEqual:kBLCWebBrowserBackString]) {
+        [self.webview goBack];
+    } else if ([title isEqual:kBLCWebBrowserForwardString]) {
+        [self.webview goForward];
+    } else if ([title isEqual:kBLCWebBrowserStopString]) {
+        [self.webview stopLoading];
+    } else if ([title isEqual:kBLCWebBrowserRefreshString]) {
+        [self.webview reload];
+    }
+}
+
+- (void) floatingToolbar:(BLCAwesomeFloatingToolbar *)toolbar didTryToPanWithOffset:(CGPoint)offset {
+    CGPoint startingPoint = toolbar.frame.origin;
+   
+        
+    CGPoint newPoint = CGPointMake(startingPoint.x + offset.x, startingPoint.y + offset.y);
+    
+    CGRect potentialNewFrame = CGRectMake(newPoint.x, newPoint.y, CGRectGetWidth(toolbar.frame), CGRectGetHeight(toolbar.frame));
+    
+    
+    
+    if (CGRectContainsRect(self.view.bounds, potentialNewFrame)) {
+        toolbar.frame = potentialNewFrame;
+    }
+}
+
+- (void) floatingToolbar:(BLCAwesomeFloatingToolbar *)toolbar didTryToPinchWithScale:(CGFloat)scale{
+    
+    
+    _zoomScale = scale;
+    
+    CGPoint newStartPoint = toolbar.frame.origin;
+    
+  
+    
+    toolbar.frame = CGRectMake(newStartPoint.x,newStartPoint.y,toolbar.frame.size.width*scale,toolbar.frame.size.height*scale);
+    
+    
+    
+    
+    
+    //toolbar.transformpo  = CGAffineTransformScale(toolbar.transform, scale, scale);
+    //scale = 1.0;
+    
+   // toolbar.frame = newFrame;
+    //if (CGRectContainsRect(self.view.bounds, newFrame)) {
+   //     toolbar.transform = CGAffineTransformScale(toolbar.transform, toolbar.frame.size.width*scale,toolbar.frame.size.height*scale);
+        
+    
+    
+   // NSLog(@"after frame: %@",self.frame);
+    
+
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    
+    NSString *URLString = textField.text;
+    
+    NSURL *URL = [NSURL URLWithString:URLString];
+   
+    
+    
+    // Check to see if there are any spaces in the URL;  Check to see if the URL is missing "."'s - Do Google Search ... Adding this
+    
+    NSRange findSpaces = [URLString rangeOfString:@" "];
+    NSRange findPeriods = [URLString rangeOfString:@"."];
+    
+    if (findSpaces.location !=NSNotFound) {
+        NSString *newSearchString = [URLString stringByReplacingOccurrencesOfString:@" " withString:@"+"];
+        URL = [NSURL URLWithString: [NSString stringWithFormat:@"http://www.google.com/search?q=%@", newSearchString]];
+        
+    }
+    
+   if (findPeriods.location ==NSNotFound) {
+        URL = [NSURL URLWithString: [NSString stringWithFormat:@"http://www.google.com/search?q=%@", URLString]];
+    }
+    
+    
+    if (!URL.scheme) {
+        
+        // The user didn't type http: or https:
+        
+        URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
+    }
+    
+  
+    
+   
+    
+    if (URL) {
+        NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+        [self.webview loadRequest:request];
+    }
+    
+    return NO;
+}
+
+#pragma mark UIWebViewDelegate
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+   self.frameCount++;
+   [self updateButtonsAndTitle];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView {
+    self.frameCount--;
+   [self updateButtonsAndTitle];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    // Let steve know, the following line wasn't in the original code snippet
+    
+    if (error.code != -999) {
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", @"Error")
+                                                    message:[error localizedDescription]
+                                                   delegate:nil
+                                          cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                                          otherButtonTitles:nil];
+    [alert show];
+
+    }
+    [self updateButtonsAndTitle];
+    self.frameCount--;
+}
+
+- (void)resetWebView {
+    
+    [self.webview removeFromSuperview];
+    
+    UIWebView *newWebView = [[UIWebView alloc]init];
+    newWebView.delegate = self;
+    [self.view addSubview:newWebView];
+    
+    self.webview = newWebView;
+    
+   
+    
+    self.textField.text = nil;
+    
+    [self updateButtonsAndTitle];
+    
+    
+    
+}
+
+#pragma mark - Miscellaneous
+
+
+
+
+
+- (void) updateButtonsAndTitle {
+    NSString *webpageTitle = [self.webview stringByEvaluatingJavaScriptFromString:@"document.title"];
+    
+    if (webpageTitle) {
+        self.title = webpageTitle;
+    } else {
+        self.title = self.webview.request.URL.absoluteString;
+    }
+
+
+if (self.frameCount > 0) {
+    [self.activityIndicator startAnimating];
+} else {
+    [self.activityIndicator stopAnimating];
+}
+
+    [self.awesomeToolbar setEnabled:[self.webview canGoBack] forButtonWithTitle:kBLCWebBrowserBackString];
+    [self.awesomeToolbar setEnabled:[self.webview canGoForward] forButtonWithTitle:kBLCWebBrowserForwardString];
+    [self.awesomeToolbar setEnabled:self.frameCount > 0 forButtonWithTitle:kBLCWebBrowserStopString];
+    [self.awesomeToolbar setEnabled:self.webview.request.URL && self.frameCount == 0 forButtonWithTitle:kBLCWebBrowserRefreshString];
+    
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+}
+@end
